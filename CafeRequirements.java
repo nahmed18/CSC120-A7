383dad1896f55a7826107abbd595f560da768eec
Your Name <you@example.com>
2025-03-27 11:32:36 -0400

    fixed the og git repository

---
+++ CafeRequirements.java
@@ -0,0 +1,6 @@
+interface CafeRequirements {
+
+    void sellCoffee(int size, int nSugarPackets, int nCreams); 
+    void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups); 
+    
+}
